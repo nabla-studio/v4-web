c1b1a7c9028fb7cd84889f7756f62fa69430c588
Jared Vu <jaredvu@gmail.com>
2024-03-12 15:33:08 -0700

    fix(tradeform): Change leverage buttons based on maxLeverage (#373)
    * Change leverage buttons based on maxLeverage
    
    * extract if statement

--- src/views/forms/TradeForm/MarketLeverageInput.tsx
+++ src/views/forms/TradeForm/MarketLeverageInput.tsx
@@ -59,6 +59,8 @@ export const MarketLeverageInput = ({
     ? BIG_NUMBERS.ONE.div(initialMarginFraction)
     : MustBigNumber(10);
 
+  const leverageOptions = maxLeverage.lt(10) ? [1, 2, 3, 4, 5] : [1, 2, 3, 5, 10];
+
   const leveragePosition = postOrderLeverage ? newPositionSide : currentPositionSide;
 
   const getSignedLeverage = (newLeverage: string | number) => {
@@ -156,7 +158,7 @@ export const MarketLeverageInput = ({
       </Styled.InputContainer>
 
       <Styled.ToggleGroup
-        items={[1, 2, 3, 5, 10].map((leverageAmount: number) => ({
+        items={leverageOptions.map((leverageAmount: number) => ({
           label: `${leverageAmount}×`,
           value: MustBigNumber(leverageAmount).toFixed(LEVERAGE_DECIMALS),
         }))}
